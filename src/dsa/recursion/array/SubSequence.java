3d9132ccc855190732e2748bffee7420ca6d58f3
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-06-13 09:51:21 +0530

    Add files via upload

--- src/dsa/recursion/array/SubSequence.java
+++ src/dsa/recursion/array/SubSequence.java
@@ -5,15 +5,36 @@ import java.util.ArrayList;
 public class SubSequence {
 	
 	public static ArrayList<String> occurences(String str){
-		return null;
 		
+		// base case
+		if(str.length() == 0) {
+			ArrayList<String> list = new ArrayList<String>();
+			list.add("");
+			return list;
+		}
+		
+		
+		// small problem
+		char firstChar = str.charAt(0);
+		ArrayList<String> result = new ArrayList<>();
+		ArrayList<String> list = occurences(str.substring(1));
+		
+		for(String i : list) {
+			// not include
+			result.add(i);
+			
+			// include
+			result.add(firstChar + i);
+		}
+		
+		return result;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		ArrayList<String> list = occurences("harsh");
-		System.out.println();
+		System.out.println(list);
 		
 	}
 
