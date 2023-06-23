7b0b8f880160ae9de64cca0b1d020bd77ba21813
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-06-23 13:36:42 +0530

    NthLargest.java

--- src/dsa/array/assignment1/NthLargest.java
+++ src/dsa/array/assignment1/NthLargest.java
@@ -4,6 +4,17 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class NthLargest {
+	
+	static void max(int arr[]) {
+		int max = arr[0];
+		
+		for (int i=0; i<arr.length; i++) {
+			if(arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		System.out.println("Nth Largest Element is : "+max);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -18,7 +29,11 @@ public class NthLargest {
 		System.out.println();
 		Arrays.sort(arr);
 		
+		max(arr);
+		
 		System.out.println("Nth Largest Element in the array is: "+(arr[arr.length-1]));
+		
+		
 	}
 
 }
