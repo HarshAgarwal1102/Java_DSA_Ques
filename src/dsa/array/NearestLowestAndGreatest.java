8d829ba38148f6d0ecb8a2230d8001a994b4f0fd
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-06-29 07:46:03 +0530

    Add files via upload

---
+++ src/dsa/array/NearestLowestAndGreatest.java
@@ -0,0 +1,32 @@
+package dsa.array;
+
+public class NearestLowestAndGreatest {
+    public static void main(String[] args) {
+        int arr[] = {10,20,30,40,50,60,70,80,90,100};
+        int low = 0;
+        int high = arr.length-1;
+        int searchElement = 70;
+        int i =0;
+        int nearestLowestElement = 0;
+        int nearestGreaterElement = 0;
+        while(i<arr.length){
+            int mid = (low+high)/2;
+            if(searchElement>arr[mid]){
+                nearestLowestElement = arr[mid];
+                low = mid + 1;
+            }
+            else if(searchElement<arr[mid]){
+                nearestGreaterElement = arr[mid];
+                high = mid -1 ;
+            }
+            else if(arr[mid]==searchElement){
+                nearestLowestElement = arr[mid-1];
+                nearestGreaterElement = arr[mid+1];
+            }
+            i++;
+        }
+        System.out.println("Search "+searchElement);
+        System.out.println("Nearest Greater Element "+nearestGreaterElement);
+        System.out.println("Nearest Lowest Element "+nearestLowestElement);
+    }
+}
