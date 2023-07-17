edf77c0d79802540f394f9d6444592ed69083959
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-07-17 19:00:12 +0530

    Create SubarrayWithZeroSum.java

---
+++ src/dsa/linkedlist/SubarrayWithZeroSum.java
@@ -0,0 +1,22 @@
+class SubarrayWithZeroSum{
+    //Function to check whether there is a subarray present with 0-sum or not.
+    static boolean findsum(int arr[],int n)
+    {
+        //Your code here
+        Map<Integer, Integer> map = new HashMap<>();
+        int sum = 0; 
+        
+        for(int i = 0; i < n; i++){
+            sum += arr[i];
+            if(sum == 0){
+                return true;
+            }
+            if(!map.containsKey(sum)){
+                map.put(sum, 1);
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
