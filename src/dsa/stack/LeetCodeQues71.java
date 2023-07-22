cfc88d45557c09228033da96f0c8c614068746e0
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-07-22 22:03:07 +0530

    Create Simplify Path

---
+++ src/dsa/stack/LeetCodeQues71.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String simplifyPath(String path) {
+      Stack<String> stack = new Stack<>();    
+        String[] directories = path.split("/"); 
+        for (String dir : directories) { 
+            if (dir.equals(".") || dir.isEmpty()) continue;
+            else if (dir.equals("..")) { 
+                if (!stack.isEmpty())
+                    stack.pop();
+            }
+            else
+                stack.push(dir);
+        }
+        return "/" + String.join("/", stack);   
+    }
+}
