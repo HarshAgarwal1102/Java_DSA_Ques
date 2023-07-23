557a2da9e84128b60ea0dfab13b0a144b596e7a9
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-07-23 18:54:08 +0530

    Create Sum of Left Leaves

---
+++ src/dsa/trees/LeetCodeQues404.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    static int result;
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        result = 0;
+
+        task(root.left, true);
+        task(root.right, false);
+
+        return result;
+    }
+
+    static void task(TreeNode root, boolean flag){
+        if(root == null) 
+            return;
+
+        if(flag && root.left==null && root.right==null) 
+            result += root.val;
+
+        task(root.left, true);
+        task(root.right, false);
+    }
+}
