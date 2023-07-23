ed3b8b45c00fa54296c9f403fb0065ce70be6c5c
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-07-23 17:25:56 +0530

    Create Binary Tree Postorder Traversal

---
+++ src/dsa/trees/LeetCodeQues145.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public void traversal(TreeNode root, List<Integer> res) {
+        if(root == null) 
+            return;
+
+        traversal(root.left, res);
+        traversal(root.right, res);
+        res.add(root.val);
+   }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        
+        traversal(root, res);
+        
+        return res;
+   }
+}
