e04e494e2407223a335388e1aef8758949b6df84
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-07-25 22:02:50 +0530

    Create GFG Ques K-thLargestSumContiguousSubarray

---
+++ src/dsa/heap/K-thLargestSumContiguousSubarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static int kthLargest(int N, int K, int[] Arr) {
+        // code here
+        PriorityQueue<Integer> pQ=new PriorityQueue<>();
+        
+        for(int i=0; i<Arr.length; i++){
+            int sum=0;
+            for(int j=i; j<Arr.length; j++){
+                sum+=Arr[j];
+                pQ.add(sum);
+                if(pQ.size()>K) 
+                    pQ.remove();
+            }
+        }
+        return pQ.peek();
+    }
+}
