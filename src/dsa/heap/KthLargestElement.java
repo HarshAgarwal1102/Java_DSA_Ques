c8492cd36a98360e693865b6c282206e15bd4fd3
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-07-25 21:44:20 +0530

    Create LeetCode Ques 215 KthLargestElement

---
+++ src/dsa/heap/KthLargestElement.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+    		for(int i=0;i<nums.length;i++){
+						pq.offer(nums[i]);
+      			if(pq.size()>k)
+        				pq.remove();
+      	}
+      	return pq.poll();
+    }
+}
