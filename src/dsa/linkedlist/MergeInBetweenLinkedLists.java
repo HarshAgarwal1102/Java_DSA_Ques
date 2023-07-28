4f42d104466da7565409c30486e67ad99a7132a3
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-07-28 21:15:18 +0530

    Create LeetCode Ques 1669 MergeInBetweenLinkedLists

---
+++ src/dsa/linkedlist/MergeInBetweenLinkedLists.java
@@ -0,0 +1,28 @@
+class Solution {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        
+        ListNode ret = list1;
+        ListNode curr = list1;
+        ListNode curr2 = list1;
+        
+        while(a>1){
+            a--;
+            curr = curr.next;
+        }
+        
+        while(b>0){
+            b--;
+            curr2 = curr2.next;
+        }
+        curr2 = curr2.next;
+        curr.next = list2;
+        
+        while(list2.next!=null){
+            list2 = list2.next;
+        } 
+        
+        list2.next = curr2;
+
+        return ret;
+    }
+}
