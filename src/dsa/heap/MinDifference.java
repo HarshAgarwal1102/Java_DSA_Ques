7fe2556d46e8d68787061d6a9015aa8f23659efb
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-07-28 11:14:22 +0530

    Create MinDifference

---
+++ src/dsa/heap/MinDifference.java
@@ -0,0 +1,33 @@
+package dsa.heap;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class MinDifference {
+    static int minDiff(int arr[]){
+        if(arr.length == 1 || arr.length == 0){
+            return 0;
+        }
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(int i :arr){
+            minHeap.offer(i);
+        }
+
+        int min = Integer.MAX_VALUE;
+
+        int top = minHeap.poll();
+        while(!minHeap.isEmpty()){
+            int current =minHeap.poll();
+            int diff = current - top;
+            min = Math.min(min, diff);
+            top= current;
+        }
+        return min;
+    }
+    
+    public static void main(String[] args) {
+        int arr[] = {15, 12, 14, 6, 9};
+        int ans = minDiff(arr);
+        System.out.println(ans);
+    }
+}
