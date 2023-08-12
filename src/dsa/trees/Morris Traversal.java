a13cd04a15b5469376abdcb7de20c1b5e6ae9bda
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-08-12 15:22:13 +0530

    Create Morris Traversal.java

---
+++ src/dsa/trees/Morris Traversal.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        ArrayList<Integer> inorder = new ArrayList <>();
+        TreeNode cur = root;
+        while (cur != null) {
+            if (cur.left==null) {
+                inorder.add(cur.val);
+                cur =cur.right;
+            }
+
+            else{
+                TreeNode prev = cur.left;
+                while (prev.right !=null && prev.right != cur) {
+                    prev = prev.right;
+                }
+                if (prev.right == null) {
+                    prev.right = cur;
+                    cur = cur.left;
+                }
+                else {
+                    prev.right = null;
+                    inorder.add(cur.val);
+                    cur = cur.right;
+                }
+            }
+        }
+
+        return inorder;
+    }
+}
