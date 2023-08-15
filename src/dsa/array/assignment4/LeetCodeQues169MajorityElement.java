cc21b2e9f4ad1a51fd79e1f052c621120ac6fafe
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-08-15 19:58:13 +0530

    Updated LeetCodeQues169MajorityElement.java

--- src/dsa/array/assignment4/LeetCodeQues169MajorityElement.java
+++ src/dsa/array/assignment4/LeetCodeQues169MajorityElement.java
@@ -27,3 +27,34 @@ public class LeetCodeQues169MajorityElement {
 		System.out.println("Majority Occuring Element is "+ans);
 	}
 }
+
+// Using Hash Map
+
+/*
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+        int size = n / 2;
+
+        for(int i = 0; i<n; i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i], map.get(nums[i]) + 1);
+            }
+            else{
+                map.put(nums[i] , 1);
+            }
+        }
+
+        for(Map.Entry<Integer, Integer> m : map.entrySet()){
+            if(m.getValue() > size){
+                return m.getKey();
+            }
+        }
+
+        return 0;
+    }
+}
+
+*/
